94ee0d536ba92ff17b9bc391820b86fc4700262d
Rajesh97266 <rajesh97266@gmail.com>
2025-02-22 07:40:22 +0530

    data-added

---
+++ src/data/data.js
@@ -0,0 +1,42 @@
+import { AiFillAppstore } from "react-icons/ai";
+import { FaMobile, FaGlobe } from "react-icons/fa";
+import { SiProgress, SiAntdesign } from "react-icons/si";
+
+// Features Data
+export const featuresData = [
+  {
+    id: 1,
+    title: "Business Strategy",
+    des: "Crafting effective business strategies to help companies grow and stay competitive in the market.",
+  },
+  {
+    id: 2,
+    icon: <AiFillAppstore />,
+    title: "App Development",
+    des: "Building high-performance mobile and web applications tailored to user needs and business goals.",
+  },
+  {
+    id: 3,
+    icon: <SiProgress />,
+    title: "SEO Optimization",
+    des: "Enhancing website visibility and ranking on search engines through best SEO practices and strategies.",
+  },
+  {
+    id: 4,
+    icon: <FaMobile />,
+    title: "Responsive Web Design",
+    des: "Making websites adjust smoothly to all screen sizes for a better user experience.",
+  },
+  {
+    id: 5,
+    icon: <SiAntdesign />,
+    title: "UX Design",
+    des: "Creating user-friendly and aesthetically pleasing designs that enhance user interaction and satisfaction.",
+  },
+  {
+    id: 6,
+    icon: <FaGlobe />,
+    title: "Hosting Websites",
+    des: "Providing reliable and secure hosting solutions to keep your website running smoothly 24/7.",
+  },
+];
