2c03b1d3564adf1d1b785b4ca3c4c1c7c8235b89
Rajesh97266 <rajesh97266@gmail.com>
2025-02-22 07:49:44 +0530

    minor-changes

--- src/components/resume/Experience.js
+++ src/components/resume/Experience.js
@@ -16,21 +16,21 @@ const Experience = () => {
         </div>
         <div className="mt-14 w-full h-[1000px] border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
           <ResumeCard
-            title="Sr. Software Engineer"
-            subTitle="Google Out Tech - (2017 - Present)"
-            result="USA"
+            title="FreeLance Developer"
+            subTitle="Upwork - (2024 - Present)"
+            result="Chennai"
             des="Google's hiring process is an important part of our culture. Googlers care deeply about their teams and the people who make them up."
           />
           <ResumeCard
-            title="Web Developer & Trainer"
-            subTitle="Apple Developer Team - (2012 - 2016)"
-            result="MALAYSIA"
+            title="Software Engineer"
+            subTitle="Expeditech INC - (2024)"
+            result="USA"
             des="A popular destination with a growing number of highly qualified homegrown graduates, it's true that securing a role in Malaysia isn't easy."
           />
           <ResumeCard
             title="Front-end Developer"
-            subTitle="Nike - (2020 - 2011)"
-            result="Oman"
+            subTitle="CMRL - (2023 - 2024)"
+            result="Chennai"
             des="The Oman economy has grown strongly over recent years, having transformed itself from a producer and innovation-based economy."
           />
         </div>
