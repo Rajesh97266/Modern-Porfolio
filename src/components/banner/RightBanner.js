8e581ef886582091c120ec9315b6bcc754fdbb35
Rajesh97266 <rajesh97266@gmail.com>
2025-03-12 17:54:54 +0530

    new-image-added

--- src/components/banner/RightBanner.js
+++ src/components/banner/RightBanner.js
@@ -1,18 +1,19 @@
-import React from 'react'
+import React from "react";
 import { bannerImg } from "../../assets/index";
-import  home from "../../assets/images/contact/home.svg"
+import home from "../../assets/images/contact/home.svg";
+import home1 from "../../assets/images/contact/home1.gif";
 
 const RightBanner = () => {
   return (
     <div className="w-full lgl:w-1/2 flex justify-center items-center relative">
       <img
-        className="w-[300px] h-[400px] lgl:w-[500px] lgl:h-[680px] z-10"
-        src={home}
+        className="w-[400px] h-[400px] lgl:w-[600px] lgl:h-[680px] z-10"
+        src={home1}
         alt="bannerImg"
       />
-      <div className="absolute bottom-0 w-[350px] h-[300px] lgl:w-[500px] lgl:h-[500px] bg-gradient-to-r from-[#1e2024] to-[#202327] shadow-shadowOne flex justify-center items-center"></div>
+      <div className="absolute bottom-0 w-[350px] h-[300px] lgl:w-[600px] lgl:h-[600px] bg-gradient-to-r from-[#1e2024] to-[#202327] shadow-shadowOne flex justify-center items-center"></div>
     </div>
   );
-}
+};
 
-export default RightBanner
+export default RightBanner;
