c1dcb5a9005ffd7889d5b8e51810189d86207fa3
Rajesh97266 <rajesh97266@gmail.com>
2025-03-15 13:08:19 +0530

    hover-added

--- src/components/projects/ProjectsCard.js
+++ src/components/projects/ProjectsCard.js
@@ -4,7 +4,14 @@ import { FaGlobe } from "react-icons/fa";
 
 const ProjectsCard = ({ title, des, src, githubLink, projectLink }) => {
   return (
-    <div className="w-full p-4 xl:px-12 h-auto xl:py-10 rounded-lg shadow-shadowOne flex flex-col bg-gradient-to-r from-bodyColor to-[#202327] group hover:bg-gradient-to-b hover:from-gray-900 hover:gray-900 transition-colors duration-1000">
+    <div
+      className="w-full p-4 xl:px-12 h-auto xl:py-10 rounded-lg shadow-shadowOne flex flex-col 
+    bg-gradient-to-r from-bodyColor to-[#202327] 
+    group 
+    hover:bg-gradient-to-b hover:from-gray-800 hover:gray-700 
+    transition-colors duration-1000 
+    hover:shadow-[0_0_35px_-2px_rgba(255,255,255,0.1)]"
+    >
       <div className="w-full h-[80%] overflow-hidden rounded-lg">
         <img
           className="w-full h-60 object-cover group-hover:scale-110 duration-300 cursor-pointer"
